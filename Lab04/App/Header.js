105929289fc756b51892f07e6ffa8f0e9fd41669
MrBig20202504 <168046771+MrBig20202504@users.noreply.github.com>
2024-05-17 12:27:16 +0700

    V5

---
+++ Lab04/App/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    Linking,
+    StyleProp,
+    TextStyle,
+    ViewStyle,
+} from 'react-native';
+import styles from './src/styles';
+const Header = () => {
+    return (
+            <Text style={styles.heading}>
+                WECELL
+            </Text>
+    );
+}
+
+
+export default Header;
